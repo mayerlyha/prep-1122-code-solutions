b2538f3d6fb345d99767c428f5379de6849b455b
Mayerly Henao Alarcon <mayerlyha@gmail.com>
2022-11-23 02:03:06 +0000

    Completed prep-javascript-arrays Mayerly Henao Alarcon

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+const firstElement = languages;
+console.log('The first element of the array is', firstElement[0]);
+
+const thirdElement = languages;
+console.log('The 3rd element of the array is', thirdElement[2]);
+
+var length = languages.length;
+console.log('The length of the array is', length);
+
+const lastElement = languages;
+console.log('The last element of the array is', lastElement[languages.length - 1]);
