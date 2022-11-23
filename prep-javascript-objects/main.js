e39625e4a82285d63b14dabdbe1dc73192c21e77
Mayerly Henao Alarcon <mayerlyha@gmail.com>
2022-11-23 00:22:13 +0000

    Completed prep-javascript-objects Mayerly Henao

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,15 @@
+var person = {
+  firstName: 'Mayerly ',
+  lastName: 'Henao',
+  hobby: 'Under Water Rugby',
+  job: 'Data Analyst',
+  previousJob: 'Manager at a Brewery'
+};
+
+var fullname = person.firstName + person.lastName;
+
+console.log(person);
+console.log('The persons name is:', fullname);
+console.log('The persons current job is', person.job);
+console.log('The persons previous job is:', person.previousJob);
+console.log('The complete person object:', person);
