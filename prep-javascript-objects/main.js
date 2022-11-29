e9befcc9955d1e460133c45eada1d5d1a4024477
Mayerly Henao Alarcon <mayerlyha@gmail.com>
2022-11-29 20:54:59 +0000

    delete commented out code

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -1,15 +1,17 @@
-var person = {
-  firstName: 'Mayerly ',
+var Mayerly = {
+  firstName: 'Mayerly',
   lastName: 'Henao',
-  hobby: 'Under Water Rugby',
-  job: 'Data Analyst',
-  previousJob: 'Manager at a Brewery'
+  hobby: 'Under Water Rugby'
 };
 
-var fullname = person.firstName + person.lastName;
+var fullname = Mayerly.firstName + ' ' + Mayerly.lastName;
 
-console.log(person);
-console.log('The persons name is:', fullname);
-console.log('The persons current job is', person.job);
-console.log('The persons previous job is:', person.previousJob);
-console.log('The complete person object:', person);
+console.log('The persons name is: ', fullname + '.');
+
+Mayerly.job = 'Business Analyst';
+console.log('The persons current job is: ' + Mayerly.job + '.');
+
+Mayerly.previousJob = 'Manager at at Brewery';
+console.log('The persons previous job is: ' + Mayerly.previousJob + '.');
+
+console.log('The complete person object: ', Mayerly);
